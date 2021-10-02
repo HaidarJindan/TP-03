2e6227a1d0d6633ade698735bccbf030abfcfc49
Haidar341 <91077398+Haidar341@users.noreply.github.com>
2021-10-02 22:27:59 +0700

    TP03.java

---
+++ TP03.java
@@ -0,0 +1,21 @@
+public class TP03 {
+  public static void main(String[] args) {
+    int a;
+    int b;
+	System.out.println("Permen Dira 10");
+	System.out.println("Teman Dira 4");
+	 // Pengisian nilai
+         a = 10;
+         b = 4;
+		
+		 // Pembagian
+         b /= a;
+        // sekarang Teman dira=4
+        System.out.println("Pembagian : " + b);
+		System.out.println("Jumlah permen ada 10, dibagi ke Dira dan 4 temannya,maka setiap orang mendapat 2 permen, tanpa ada sisa.");
+         
+    }
+
+}
+
+       
